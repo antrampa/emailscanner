e0a415cf26706618fc767d695bdd4b287ef104cc
Antonis Zalonis <a.zalonis@sigmasolutions.gr>
2017-10-10 13:29:09 +0300

    run

--- scanner/Program.cs
+++ scanner/Program.cs
@@ -14,69 +14,73 @@ namespace scanner
     {
         static void Main(string[] args)
         {
-            IReaderController reader = new ReaderController();
-            IWebScannerController scaner = new WebScannerController();
-            IWriterController writerController = new WriterController();
-
-            var db = new MainContext();
-
-            var savedLinks = reader.ReadLinksFromDB(); // reader.ReadLinks();
-            var linkDao = new LinksDao();
-            foreach (var savedLink in savedLinks)
+            while (true)
             {
-                Console.WriteLine(savedLink);
 
-                var links = scaner.FindLinks(savedLink, savedLink);
+                IReaderController reader = new ReaderController();
+                IWebScannerController scaner = new WebScannerController();
+                IWriterController writerController = new WriterController();
 
+                var db = new MainContext();
 
-                writerController.WriteLinks(links);
-
-                //TODO - Read new links 
-
-                //TODO Mails
-                var mails = scaner.FindMails(savedLink, savedLink);
-                writerController.WriteEmails(mails);
-
-                foreach (var link in links)
+                var savedLinks = reader.ReadLinksFromDB(); // reader.ReadLinks();
+                var linkDao = new LinksDao();
+                foreach (var savedLink in savedLinks)
                 {
-                    mails = scaner.FindMails(link, "");
+                    Console.WriteLine(savedLink);
+
+                    var links = scaner.FindLinks(savedLink, savedLink);
+
+
+                    writerController.WriteLinks(links);
+
+                    //TODO - Read new links 
+
+                    //TODO Mails
+                    var mails = scaner.FindMails(savedLink, savedLink);
                     writerController.WriteEmails(mails);
+
+                    foreach (var link in links)
+                    {
+                        mails = scaner.FindMails(link, "");
+                        writerController.WriteEmails(mails);
+                    }
+
+
+                    ////TODO H1
+                    //var h1Texts = scaner.FindH1(savedLink, savedLink);
+                    //writerController.WriteH1(h1Texts);
+
+                    //foreach (var link in links)
+                    //{
+                    //    h1Texts = scaner.FindH1(link, "");
+                    //    writerController.WriteH1(mails);
+                    //}
+
+                    //Save Link as Scanned 
+                    var scannedLink = new Link(savedLink, true);
+                    linkDao.Add(scannedLink);
+
                 }
 
 
-                ////TODO H1
-                //var h1Texts = scaner.FindH1(savedLink, savedLink);
-                //writerController.WriteH1(h1Texts);
+                //var links = scaner.FindLinks("https://www.xo.gr/dir-az/A/Antallaktika-Aftokiniton-Eidi-kai-Axesouar/", "https://www.xo.gr");
 
-                //foreach (var link in links)
+
+                //writerController.WriteLinks(links);
+
+                ////TODO - Read new links 
+
+                ////TODO Mails
+                //var mails = scaner.FindMails("https://www.xo.gr/dir-az/A/Antallaktika-Aftokiniton-Eidi-kai-Axesouar/", "https://www.xo.gr");
+                //writerController.WriteEmails(mails);
+
+                //foreach(var link in links)
                 //{
-                //    h1Texts = scaner.FindH1(link, "");
-                //    writerController.WriteH1(mails);
+                //    mails = scaner.FindMails(link, "");
+                //    writerController.WriteEmails(mails);
                 //}
-
-                //Save Link as Scanned 
-                var scannedLink = new Link(savedLink, true);
-                linkDao.Add(scannedLink);
-
             }
-
-            
-            //var links = scaner.FindLinks("https://www.xo.gr/dir-az/A/Antallaktika-Aftokiniton-Eidi-kai-Axesouar/", "https://www.xo.gr");
-
-            
-            //writerController.WriteLinks(links);
-
-            ////TODO - Read new links 
-
-            ////TODO Mails
-            //var mails = scaner.FindMails("https://www.xo.gr/dir-az/A/Antallaktika-Aftokiniton-Eidi-kai-Axesouar/", "https://www.xo.gr");
-            //writerController.WriteEmails(mails);
-
-            //foreach(var link in links)
-            //{
-            //    mails = scaner.FindMails(link, "");
-            //    writerController.WriteEmails(mails);
-            //}
         }
     }
 }
